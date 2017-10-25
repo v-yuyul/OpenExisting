6a4bf8972fd42b1b8ca7a2dccce13e85e3292412
Lizzy Li (CSI Interfusion Inc) <v-yanlli@microsoft.com>
2017-10-25 13:32:10 +0800

    update

---
+++ OpenExisting11ChangeTo20/AspNetCoreWebApp_IndAuth10_Net461/AspNetCoreWebApp_IndAuth10_Net461/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApp_IndAuth10_Net461.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
