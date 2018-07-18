3241d7cd7f32241732594fc9dbf1d6b233c92353
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2018-07-18 15:16:52 +0800

    add OpenExistingDev14WapSln

---
+++ OpenExistingDev14WapSln/WebAPIWithIndAuthCs/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIWithIndAuthCs.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OpenExistingDev14WapSln/WebAPIWithIndAuthCs/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebAPIWithIndAuthCs.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
