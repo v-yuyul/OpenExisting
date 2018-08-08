8739e9b1c280475aa33dd349384c4f2964bfab25
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2018-08-08 15:55:18 +0800

    update dev14wap

---
+++ Dev14U3/OpenExistingDev14WapSln/MVC5WithIndCs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5WithIndCs.Startup))]
+namespace MVC5WithIndCs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
