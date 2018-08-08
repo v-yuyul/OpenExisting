2447760becbe9490bc3a34d286fa93561b8e2ab2
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2018-08-08 13:26:25 +0800

    update

---
+++ OpenExisting11ChangeTo20/AspNetCoreWebApp_IndAuth10_Net461/AspNetCoreWebApp_IndAuth10_Net461/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApp_IndAuth10_Net461.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ OpenExisting11ChangeTo20/WebApp_NetFW_Ind10_OnNet461/WebApp_NetFramework_Ind10_OnNet461/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_NetFramework_Ind10_OnNet461.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
