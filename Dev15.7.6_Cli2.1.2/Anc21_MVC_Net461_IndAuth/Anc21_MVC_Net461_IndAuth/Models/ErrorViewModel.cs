792a144f19fff23f236bf5c1b05f927900863a23
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2018-08-15 14:56:19 +0800

    add 2.1_UpdateToLatest

---
+++ Dev15.7.6_Cli2.1.2/Anc21_MVC_Net461_IndAuth/Anc21_MVC_Net461_IndAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anc21_MVC_Net461_IndAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
