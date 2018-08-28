ffc2a00b81af14c9ba5a2d6081d457110fed4681
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2018-08-28 17:10:27 +0800

    Update Core21_updateToLatest

--- Dev15.7.6_Cli2.1.2/Anc21_RazorPagesWithRazorClassLib_Core_IndAuth/RazorClassLib_Core21/Areas/MyFeature/Pages/Page1.cshtml.cs
+++ Dev15.7.6_Cli2.1.2/Anc21_RazorPagesWithRazorClassLib_Core_IndAuth/RazorClassLib_Core21/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorClassLib.MyFeature.Pages
+namespace RazorClassLib_Core21.MyFeature.Pages
 {
     public class Page1Model : PageModel
     {
