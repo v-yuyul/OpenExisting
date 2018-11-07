1ca1f9076e4f3d4078a64d22bfaa1a2d1faeef46
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2018-11-07 13:38:34 +0800

    Update OpenSolutionFromUNC

--- MultiProIn15.9/WebFormsIndAuth/Properties/AssemblyInfo.cs
+++ MultiProIn15.9/WebFormsIndAuth/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("235b50fb-220c-4385-aa2d-7892a10bba38")]
+[assembly: Guid("71272e47-cc0d-42c1-997f-3a51b3c9d15c")]
 
 // Version information for an assembly consists of the following four values:
 //
