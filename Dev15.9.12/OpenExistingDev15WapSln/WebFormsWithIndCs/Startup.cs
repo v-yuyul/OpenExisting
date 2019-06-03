3f6cf04ac646ade4d5dd75e525452b156e5175ec
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2019-06-03 16:56:07 +0800

    Add Dev15 WAP and Website

---
+++ Dev15.9.12/OpenExistingDev15WapSln/WebFormsWithIndCs/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsWithIndCs.Startup))]
+namespace WebFormsWithIndCs
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
