378930b5acf9376b57c4229be6620c51383d255c
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2019-08-08 11:21:34 +0800

    Add Anc22_Core

---
+++ Dev15.9.14_Cli2.2.6/Anc22_RazorWithRCLib_Core_Ind/Anc22_RazorWithRCLib_Core_Ind/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Anc22_RazorWithRCLib_Core_Ind.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Dev15.9.14_Cli2.2.6/Anc22_RazorWithRCLib_Core_Ind/Anc22_RazorWithRCLib_Core_Ind/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Anc22_RazorWithRCLib_Core_Ind.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
