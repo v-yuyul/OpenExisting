b0a7787cbda0c0297ba4a9cb072ad89017f25a40
Cindy Liu <v-yuyul@microsoft.com>
2020-07-31 13:16:16 +0800

    remove 16.3.8 and update 16.6.5 instead

---
+++ 16.6.5/Anc31_MVCWithRCL_Ind/Anc31_MVCWithRCL_Ind/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Anc31_MVCWithRCL_Ind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
